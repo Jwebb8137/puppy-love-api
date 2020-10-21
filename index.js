a5a11ee9e1fa0137e16c7867acabb03c80bad6ca
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-10-20 20:24:43 -0700

    heroku

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const { chatToken, videoToken } = require('./tokens');
 //MIDDLEWARE
 
 app.use(cors())
+app.options('*', cors());
 app.use(express.json({ limit: '50mb' })); //req.body
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({ extended: false }));

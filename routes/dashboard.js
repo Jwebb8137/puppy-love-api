23f54cbe2cd6a8b4903849ff60b1f62771b8a994
JoshTriangleTechDeveloper <joshwebb8137@gmail.com>
2020-10-28 15:17:26 -0700

    refactor

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const authorization = require("../middleware/authorization");
 const pool = require("../db");
 const authorization = require("../middleware/authorization");
 
